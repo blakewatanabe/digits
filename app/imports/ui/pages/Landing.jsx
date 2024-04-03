cbc74a62e4d199221a1016ceaaea54e9db621097
Blake Watanabe <blakewanabi@gmail.com>
2024-04-03 11:01:03 -1000

    clean up

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,17 +6,17 @@ import { PeopleFill, FileEarmarkTextFill, Calendar2CheckFill } from 'react-boots
 const Landing = () => (
   <Container id="landing-page" className="py-3">
     <Row className="align-middle text-center">
-      <Col>
+      <Col className="pt-5">
         <PeopleFill size="100px" />
         <h1>Multiple Users</h1>
         <h5>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h5>
       </Col>
-      <Col>
+      <Col className="pt-5">
         <FileEarmarkTextFill size="100px" />
         <h1>Contact Details</h1>
         <h5>For each contact, you can save their name, address, and phone number.</h5>
       </Col>
-      <Col>
+      <Col className="pt-5">
         <Calendar2CheckFill size="100px" />
         <h1>Timestamped Notes</h1>
         <h5>Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</h5>
